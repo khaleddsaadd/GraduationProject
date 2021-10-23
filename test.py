54b73096d09fed82475d9872ff9a9edbbbff0548
khaleddsaadd <74107324+khaleddsaadd@users.noreply.github.com>
2021-10-23 18:11:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,15 @@ import nltk
 from nltk.chat.util import Chat, reflections
 from gtts import gTTS
 import os
-
+import random
+import re
+def textToSpeach(text):
+    myobj = gTTS(text=text, lang='en', slow=False)
+    # Saving the converted audio in a mp3 file named
+    # welcome 
+    myobj.save("welcome.mp3")
+    # Playing the converted file
+    os.system("welcome.mp3")
 reflections = {
   "i am"       : "you are",
   "i was"      : "you were",
@@ -107,18 +115,102 @@ pairs = [
         ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
     ],
 ]
-def textToSpeach(text):
-    myobj = gTTS(text=text, lang='en', slow=False)
-    # Saving the converted audio in a mp3 file named
-    # welcome 
-    myobj.save("welcome.mp3")
-    # Playing the converted file
-    os.system("welcome.mp3")
+class Chat:
+    def __init__(self, pairs, reflections={}):
+        """
+        Initialize the chatbot.  Pairs is a list of patterns and responses.  Each
+        pattern is a regular expression matching the user's statement or question,
+        e.g. r'I like (.*)'.  For each such pattern a list of possible responses
+        is given, e.g. ['Why do you like %1', 'Did you ever dislike %1'].  Material
+        which is matched by parenthesized sections of the patterns (e.g. .*) is mapped to
+        the numbered positions in the responses, e.g. %1.
+
+        :type pairs: list of tuple
+        :param pairs: The patterns and responses
+        :type reflections: dict
+        :param reflections: A mapping between first and second person expressions
+        :rtype: None
+        """
+
+        self._pairs = [(re.compile(x, re.IGNORECASE), y) for (x, y) in pairs]
+        self._reflections = reflections
+        self._regex = self._compile_reflections()
+
+    def _compile_reflections(self):
+        sorted_refl = sorted(self._reflections, key=len, reverse=True)
+        return re.compile(
+            r"\b({})\b".format("|".join(map(re.escape, sorted_refl))), re.IGNORECASE
+        )
+
+    def _substitute(self, str):
+        """
+        Substitute words in the string, according to the specified reflections,
+        e.g. "I'm" -> "you are"
+
+        :type str: str
+        :param str: The string to be mapped
+        :rtype: str
+        """
+
+        return self._regex.sub(
+            lambda mo: self._reflections[mo.string[mo.start() : mo.end()]], str.lower()
+        )
+
+    def _wildcards(self, response, match):
+        pos = response.find("%")
+        while pos >= 0:
+            num = int(response[pos + 1 : pos + 2])
+            response = (
+                response[:pos]
+                + self._substitute(match.group(num))
+                + response[pos + 2 :]
+            )
+            pos = response.find("%")
+        return response
+
+    def respond(self, str):
+        """
+        Generate a response to the user input.
+
+        :type str: str
+        :param str: The string to be mapped
+        :rtype: str
+        """
+
+        # check each pattern
+        for (pattern, response) in self._pairs:
+            match = pattern.match(str)
+
+            # did the pattern match?
+            if match:
+                resp = random.choice(response)  # pick a random response
+                resp = self._wildcards(resp, match)  # process wildcards
+
+                # fix munged punctuation at the end
+                if resp[-2:] == "?.":
+                    resp = resp[:-2] + "."
+                if resp[-2:] == "??":
+                    resp = resp[:-2] + "?"
+                return resp
+
+
+    # Hold a conversation with a chatbot
+    def converse(self, quit="quit"):
+        user_input = ""
+        while user_input != quit:
+            user_input = quit
+            try:
+                user_input = input(">")
+            except EOFError:
+                print(user_input)
+            if user_input:
+                while user_input[-1] in "!.":
+                    user_input = user_input[:-1]
+                x=self.respond(user_input)
+                textToSpeach(x)
 def chat():
     chat = Chat(pairs, reflections)
     chat.converse()
-#initiate the conversation
 if __name__ == "__main__":
     mytext = 'Welcome'
-    textToSpeach(mytext)
     chat()
