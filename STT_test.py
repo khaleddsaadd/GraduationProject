2d08dbcc4f6aab6e94b714ca22ba0780cb947aaf
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-10-24 22:13:57 +0200

    Update STT_test.py

--- STT_test.py
+++ STT_test.py
@@ -4,12 +4,8 @@ r = sr.Recognizer()
 with sr.Microphone() as source:
     # read the audio data from the default microphone
     audio_data = r.record(source, duration=5)
-    
     print("Recognizing...")
     # convert speech to text
-
-    text = r.recognize_google(audio_data, language="es-ES")
-    #text = r.recognize_google(audio_data)
-
+    text = r.recognize_google(audio_data)
     print(text)
-    
+    text = r.recognize_google(audio_data, language="es-ES")
