2c95bb0b8b015390e676f7fc24485da0c9bc39ff
Khaled Saad <74107324+khaleddsaadd@users.noreply.github.com>
2021-12-16 15:39:26 +0200

    Merging Sequence of emotions extracted from frames

--- VideoEmotions.py
+++ VideoEmotions.py
@@ -1,17 +1,31 @@
-import cv2     # for capturing videos
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
 import math   # for mathematical operations
 import matplotlib.pyplot as plt    # for plotting the images
 import pandas as pd
-from keras.preprocessing import image   # for preprocessing the images
-import numpy as np    # for mathematical operations
 from keras.utils import np_utils
 from skimage.transform import resize   # for resizing images
 
 count = 0
-videoFile = r"C:\xampp\htdocs\Pruney\Demo.mp4"
+videoFile = r"Videos/Demo.mp4"
 cap = cv2.VideoCapture(videoFile)   # capturing the video from the given path
 frameRate = cap.get(5) #frame rate
 x=1
+
+
+face_classifier=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = load_model('EmotionDetectionModel.h5')
+
+class_labels=['Angry','Happy','Neutral','Sad','Surprise']
+
+# cap=cv2.VideoCapture(0)
+#Array to store the sequence of emotions
+Emotions = []
+
 while(cap.isOpened()):
     frameId = cap.get(1) #current frame number
     ret, frame = cap.read()
@@ -19,6 +33,33 @@ while(cap.isOpened()):
         break
     if (frameId % math.floor(frameRate) == 0):
         filename ="frame%d.jpg" % count;count+=1
-        cv2.imwrite(filename, frame)
+    labels=[]
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=face_classifier.detectMultiScale(gray,1.3,5)
+   
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray=gray[y:y+h,x:x+w]
+        roi_gray=cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
+
+        if np.sum([roi_gray])!=0:
+            roi=roi_gray.astype('float')/255.0
+            roi=img_to_array(roi)
+            roi=np.expand_dims(roi,axis=0)
+
+            preds=classifier.predict(roi)[0]
+            label=class_labels[preds.argmax()]
+            label_position=(x,y)
+            Emotions.append(label)
+            #print(label)
+            cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        else:
+            cv2.putText(frame,'No Face Found',(20,20),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+    
+    cv2.imshow('Emotion Detector',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 cap.release()
 print ("Done!")
+print(Emotions)
+cv2.destroyAllWindows()
