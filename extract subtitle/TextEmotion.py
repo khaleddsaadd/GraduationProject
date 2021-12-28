54a3bc9a7edad2cb0b9444713b07c9b7667ec4a1
maielghandour <74146386+maielghandour@users.noreply.github.com>
2021-12-28 19:46:30 +0200

    Seq. of emotions from subtitles

--- extract subtitle/TextEmotion.py
+++ extract subtitle/TextEmotion.py
@@ -1,3 +1,5 @@
+import text2emotion as te
+
 with open('hobbsandshaw.srt') as file:
     subtitle = file.readlines()
     sub_list = [subtitle[i : i+4] for i in range(0, len(subtitle), 4)]
@@ -15,3 +17,12 @@ for x in this_dict:
   #print(this_dict[x])
   subtitle.append(this_dict[x])
 print(subtitle)
+
+
+
+emotions=[]
+for E in subtitle:
+  max_key=max(te.get_emotion(E),key=te.get_emotion(E).get)
+  emotions.append(max_key)
+  #print(max_key)
+print(emotions) 
