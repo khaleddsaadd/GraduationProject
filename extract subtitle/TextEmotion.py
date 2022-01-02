50530061a4d1febb39f205592b071be65a6eadd3
maielghandour <74146386+maielghandour@users.noreply.github.com>
2022-01-02 17:57:38 +0200

    seq of subtitles

--- extract subtitle/TextEmotion.py
+++ extract subtitle/TextEmotion.py
@@ -2,8 +2,12 @@ import os
 import re
 import pysrt
 import text2emotion as te
+from collections import Counter
+import csv
+from csv import DictWriter
 
-folderpath = r"Sub" # make sure to put the 'r' in front
+
+folderpath = r"Subtitles" # make sure to put the 'r' in front
 filepaths  = [os.path.join(folderpath, name) for name in os.listdir(folderpath)]
 all_files = []
 S=[]
@@ -15,11 +19,34 @@ for path in filepaths:
 
     for sub in subs:
         S.append(sub.text)
-    #print(S)
     for Emotion in S:
         max_key=max(te.get_emotion(Emotion),key=te.get_emotion(Emotion).get)
         emotions.append(max_key)
-    print(emotions)
+    print("Original Sequence ",emotions)
+    a= Counter()
+    b = 0
+    n = 5
+    f=[]
+    for x in emotions :
+      b+=1
+      a [x]+=1
+      
+      if b%n == 0:
+        max_key1 = max(a, key=a.get)
+        f.append(max_key1)
+        a= Counter()
+    print("\n*************************************************************************************************")
+    print("Minimized Sequence ",f) 
+
+    fieldnames = ['Movie_Name','Emotions']
+    op={'Movie_Name':path,'Emotions':f }
+
+    with open(r'Emotions Dataset.csv','a') as csv_file:
+        dictwriter_object = DictWriter(csv_file, fieldnames=fieldnames)
+        dictwriter_object.writerow(op)
+        csv_file.close()
+
     S*= 0
     emotions*=0
+    f*=0
     print("---------------------------------------------------------------------------")
