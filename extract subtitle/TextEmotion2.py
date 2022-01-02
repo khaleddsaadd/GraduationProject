a973dbf21a9a2c80195f89721facd1eddb14a562
Khaled Saad <74107324+khaleddsaadd@users.noreply.github.com>
2022-01-02 22:42:35 +0200

    Updated Probability On Subtitles

--- extract subtitle/TextEmotion2.py
+++ extract subtitle/TextEmotion2.py
@@ -1,5 +1,29 @@
+# import pandas as pd 
+# import nltk
+# from SeqMarkovChain import*
+
+# df = pd.read_csv('Emotions Dataset.csv')
+# df['Emotions'] =  df['Emotions'].apply(lambda x: x.replace('[','').replace(']','')) 
+# df['Emotions'] =  df['Emotions'].apply(lambda x: x.replace('"','').replace('"','')) 
+# df['Emotions'] =  df['Emotions'].apply(lambda x: x.replace(''','').replace(''','')) 
+
+# #convert the string columns to int
+# df['Emotions'] = df['Emotions']
+
+# x = df.Emotions[0]
+# x = x + " "
+# x = df.Emotions[1]
+
+# x = x.replace(',', '')
+# x = x.replace("'", "")
+# print(x)
+# arr = a_list = nltk.word_tokenize(x)
+# print (arr)
+# SeqMarkovChain(arr)
+
 import pandas as pd
 from SeqMarkovChain import*
+import re
 import nltk
 import numpy as np
 dataset = pd.read_csv("Emotions Dataset.csv")
