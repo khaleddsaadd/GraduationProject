8cc0ee566e0e336fd85f315deab8db61f9899936
maielghandour <74146386+maielghandour@users.noreply.github.com>
2022-03-06 22:18:27 +0200

    Blur Scene Part2

--- Video Emotions/BlurScene.py
+++ Video Emotions/BlurScene.py
@@ -4,7 +4,6 @@ from moviepy.video.compositing.concatenate import concatenate_videoclips
 from skimage.filters import gaussian
 from moviepy.editor import VideoFileClip, clips_array, concatenate_videoclips
 from moviepy.editor import VideoFileClip, concatenate_videoclips
-import pygame
 def blur(image):
     return cv2.medianBlur(image, 101)
 def BlurringPart(Start,End):
@@ -14,3 +13,20 @@ def BlurringPart(Start,End):
     clip2 = VideoFileClip("VideoTest.mp4").subclip(Start,End)
     clip3 = VideoFileClip("VideoTest.mp4").subclip(PostViolence,15)
     clip2_blurred = clip2.fl_image(blur)
+    final_clip = concatenate_videoclips([clip1,clip2_blurred,clip3])
+    final_clip.write_videofile("my_concatenation.mp4")
+    cap = cv2.VideoCapture('my_concatenation.mp4')
+    if (cap.isOpened()== False): 
+        print("Error opening video  file")
+    while(cap.isOpened()): 
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow('Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else: 
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+BlurringPart(5,10)
+
