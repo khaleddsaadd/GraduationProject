367416aa6f48a2196f7c02eb4255bc5d55ae35c2
maielghandour <74146386+maielghandour@users.noreply.github.com>
2022-05-05 00:10:36 +0200

    Sequence of emotion from pre-violence dataset

--- Subtitle Emotions/Sub_MarkovChain.py
+++ Subtitle Emotions/Sub_MarkovChain.py
@@ -24,11 +24,29 @@ def MarkovModel(csvName):
     # print(arr)
     return SeqMarkovChain(arr)
 
-print("Violence Subtitles Dataset")
-print(MarkovModel('Subtitle Emotions\Datasets\\PreViolence_Subtitles_Dataset.csv'))
+def DatasetFullSeq(csvName):
+    df = pd.read_csv(csvName)
+    df['Emotions'] =  df['Emotions'].apply(lambda x: x.replace('[','').replace(']','')) 
+    df['Emotions'] =  df['Emotions'].apply(lambda x: x.replace('"','').replace('"','')) 
+    df['Emotions'] =  df['Emotions'].apply(lambda x: x.replace(''','').replace(''','')) 
 
-# print("Post-Violence Subtitles Dataset")
-# print(func('Subtitle Emotions\Datasets\PostViolence_Subtitles_Dataset.csv'))
+    #convert the string columns to int
+    df['Emotions'] = df['Emotions']
+    x= ""
+    for index,row in df.iterrows():
+        x = x + df.Emotions[index]
+        x = x + " "
+    x = x.replace(',','')
+    x = x.replace("'", "")
+    arr = nltk.word_tokenize(x)
+    return arr
+    
 
-print("Non Violence Subtitles Dataset")
-print(MarkovModel('Subtitle Emotions\Datasets\\NonViolence_Subtitles_Dataset.csv'))
+# print("Violence Subtitles Dataset")
+# print(MarkovModel('Subtitle Emotions\Datasets\\PreViolence_Subtitles_Dataset.csv'))
+
+# # print("Post-Violence Subtitles Dataset")
+# # print(func('Subtitle Emotions\Datasets\PostViolence_Subtitles_Dataset.csv'))
+
+# print("Non Violence Subtitles Dataset")
+# print(MarkovModel('Subtitle Emotions\Datasets\\NonViolence_Subtitles_Dataset.csv'))
