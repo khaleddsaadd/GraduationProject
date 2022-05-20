80115a6975b2e2077e22da509820daafd0faa9a7
maielghandour <74146386+maielghandour@users.noreply.github.com>
2022-05-21 01:02:35 +0200

    Working on Blurring Violent scene

--- Video Emotions/BlurScene.py
+++ Video Emotions/BlurScene.py
@@ -1,32 +1,51 @@
 import cv2
+import csv
 import numpy as np
+from csv import reader
+
+import pandas as pd
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from skimage.filters import gaussian
 from moviepy.editor import VideoFileClip, clips_array, concatenate_videoclips
 from moviepy.editor import VideoFileClip, concatenate_videoclips
+
 def blur(image):
     return cv2.medianBlur(image, 101)
-def BlurringPart(Start,End):
+def BlurringPart(MovieName,Start,End):
     BeforeViolnce = Start-1
     PostViolence = End+1
-    clip1 = VideoFileClip("VideoTest.mp4").subclip(1,BeforeViolnce)
-    clip2 = VideoFileClip("VideoTest.mp4").subclip(Start,End)
-    clip3 = VideoFileClip("VideoTest.mp4").subclip(PostViolence,15)
+    clip = VideoFileClip(MovieName,".mp4")
+    # x =int(clip.duration)
+    # count=0
+    # clips=[]
+    # for i in range(0,clip.duration()):
+    #     if(i==Start[i]):
+    #         clip[i].subclip(Start[i],End[i])
+    #         clip[i].preview()
+    clip1 = VideoFileClip(MovieName,".mp4").subclip(clip.start,BeforeViolnce)
+    clip2 = VideoFileClip(MovieName,".mp4").subclip(Start,End)
+    clip3 = VideoFileClip(MovieName,".mp4").subclip(PostViolence,clip.end)
     clip2_blurred = clip2.fl_image(blur)
     final_clip = concatenate_videoclips([clip1,clip2_blurred,clip3])
-    final_clip.write_videofile("my_concatenation.mp4")
-    cap = cv2.VideoCapture('my_concatenation.mp4')
-    if (cap.isOpened()== False): 
-        print("Error opening video  file")
-    while(cap.isOpened()): 
-        ret, frame = cap.read()
-        if ret == True:
-            cv2.imshow('Frame', frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-        else: 
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-BlurringPart(5,10)
 
+def Violence_CSV(MovieName):
+    Start=[]
+    End=[]
+    print("Maioi: ",MovieName)
+    MovieName_CSV = MovieName + ".csv"
+    print(MovieName_CSV)
+    with open(MovieName_CSV, 'r') as my_file:
+        file_csv = reader(my_file)
+        head = next(file_csv)
+        if head is not None:
+            for i in file_csv:
+                Start.append(i[0])
+                End.append(i[1])
+    print(Start)
+    print(End)
+    # for i in range (len(Start)):
+    #     print(Start[i])
+    #     BlurringPart(MovieName,int(Start[i]),int(End[i]))
+    # BlurringPart(MovieName,Start,End)
+Violence_CSV("F:\Films and Series\[EgyBest].Mr.Nobody.2009.BluRay.720p.x264.mp4")
+# BlurringPart(MovieName,int(Start[i]),int(End[i]))
